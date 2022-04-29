b79ab9396702411d056a8f8e44a323235aa6d0b6
GuilhermeBraz <gvcerveirabraz@gmail.com>
2022-04-29 09:52:27 -0300

    fixed client
    Co-authored-by: Antônio Aldísio <aldisiofilho@gmail.com>

--- client.py
+++ client.py
@@ -7,6 +7,7 @@ from tkinter import simpledialog
 HOST = '127.0.0.1'
 PORT = 9090
 
+
 class Client:
 
     def __init__(self, host, port):
@@ -18,7 +19,8 @@ class Client:
         msg = tkinter.Tk()
         msg.withdraw()
 
-        self.nickname = simpledialog.askstring("Nickname", "Escolha seu apelido:")
+        self.nickname = simpledialog.askstring(
+            "Nickname", "Escolha seu apelido:")
         self.gui_done = False
         self.running = True
 
@@ -27,28 +29,31 @@ class Client:
         gui_thread.start()
         receive_thread.start()
 
-
     # sem funcionalidade so a interface front-end
+
     def gui_loop(self):
         self.win = tkinter.Tk()
-        self.win.configure(bg ="lightblue")
+        self.win.configure(bg="lightblue")
 
         self.chat_label = tkinter.Label(self.win, text="Chat", bg='lightblue')
         self.chat_label.configure(font=("Courier", 12))
         self.chat_label.pack(padx=20, pady=5)
 
-        self.chat_text = tkinter.scrolledtext.ScrolledText(self.win, width=40, height=10)
+        self.chat_text = tkinter.scrolledtext.ScrolledText(
+            self.win, width=40, height=10)
         self.chat_text.pack(padx=20, pady=5)
 
-        self.input_label = tkinter.Label(self.win, text="Mensagem", bg='lightblue')
+        self.input_label = tkinter.Label(
+            self.win, text="Mensagem", bg='lightblue')
         self.input_label.pack(padx=20, pady=5)
 
-        # maybe is input_text instead of input_area
-        self.input_area = tkinter.Entry(self.win, width=40)
-        self.input_area.configure(font=("Courier", 12))
-        self.input_area.pack(padx=20, pady=5)
+        # maybe is input_text instead of input_text
+        self.input_text = tkinter.Entry(self.win, width=40)
+        self.input_text.configure(font=("Courier", 12))
+        self.input_text.pack(padx=20, pady=5)
 
-        self.send_button = tkinter.Button(self.win, text="Enviar", command=self.write)
+        self.send_button = tkinter.Button(
+            self.win, text="Enviar", command=self.write)
         self.send_button.pack(padx=20, pady=5)
 
         self.gui_done = True
@@ -57,16 +62,17 @@ class Client:
         self.win.mainloop()
 
     def write(self):
-        message = f"{self.nickname}: {self.input_area.get('1.0', 'end')}"
+        # message = f"{self.nickname}: {self.input_text.get('1.0', 'end')}"
+        message = f"{self.nickname}: {self.input_text.get()}"
         self.socket.send(message.encode('utf-8'))
-        self.input_area.delete('1.0', 'end')
+        self.input_text.delete(first=0, last='end')
+        # self.input_text.delete('1.0', 'end')
 
     def stop(self):
         self.running = False
         self.win.destroy()
         self.socket.close()
         exit(0)
-
 
     def receive(self):
         while self.running:
@@ -76,15 +82,16 @@ class Client:
                     self.socket.send(message.encode('utf-8'))
                 else:
                     if self.gui_done:
-                        self.text_area.config(state='normal')
+                        self.chat_text.config(state='normal')
                         self.chat_text.insert('end', message + '\n')
-                        self.text_area.yview('end')
-                        self.text_area.config(state='disabled')
+                        self.chat_text.yview('end')
+                        self.chat_text.config(state='disabled')
             except ConnectionAbortedError():
                 break
             except:
                 print("Error")
                 self.sock.close()
                 break
+
 
 client = Client(HOST, PORT)
