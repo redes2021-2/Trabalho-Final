1c170c2ba28c5e9b8460eacfc739d9da94cdec33
GuilhermeBraz <gvcerveirabraz@gmail.com>
2022-04-29 12:12:13 -0300

    adicionado comentarios e arrumado bugs
    Co-authored-by: Antônio Aldísio <aldisiofilho@gmail.com>
    Co-authored-by: Carlacangussu <40703846+Carlacangussu@users.noreply.github.com>

--- client.py
+++ client.py
@@ -1,9 +1,13 @@
 import socket
 import threading
+# imports de biblioteca para GUI
+# se não tiver o tkinter, instale-o com o comando:
+# sudo apt install python3-tk
 import tkinter
 import tkinter.scrolledtext
 from tkinter import simpledialog
 
+# Declarar ip host e porta
 HOST = '127.0.0.1'
 PORT = 9090
 
@@ -11,10 +15,11 @@ PORT = 9090
 class Client:
 
     def __init__(self, host, port):
-        self.host = host
-        self.port = port
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((self.host, self.port))
+        # Etapa de conexão
+        self.socket = socket.socket(
+            socket.AF_INET, socket.SOCK_STREAM)     # criar socket
+        # conectar ao host
+        self.socket.connect((host, port))
 
         msg = tkinter.Tk()
         msg.withdraw()
@@ -24,64 +29,75 @@ class Client:
         self.gui_done = False
         self.running = True
 
+        # thread para receber mensagens
         gui_thread = threading.Thread(target=self.gui_loop)
         receive_thread = threading.Thread(target=self.receive)
         gui_thread.start()
         receive_thread.start()
 
     # sem funcionalidade so a interface front-end
-
     def gui_loop(self):
+        # criar janela
         self.win = tkinter.Tk()
         self.win.configure(bg="lightblue")
+        self.win.title("Chat")
+        self.win.geometry("400x400")
 
+        # input text
         self.chat_label = tkinter.Label(self.win, text="Chat", bg='lightblue')
         self.chat_label.configure(font=("Courier", 12))
         self.chat_label.pack(padx=20, pady=5)
 
+        # campo de texto
         self.chat_text = tkinter.scrolledtext.ScrolledText(
             self.win, width=40, height=10)
         self.chat_text.pack(padx=20, pady=5)
 
+        # label para input de mensagem
         self.input_label = tkinter.Label(
             self.win, text="Mensagem", bg='lightblue')
         self.input_label.pack(padx=20, pady=5)
 
-        # maybe is input_text instead of input_text
+        # campo de texto de entrada
         self.input_text = tkinter.Entry(self.win, width=40)
         self.input_text.configure(font=("Courier", 12))
         self.input_text.pack(padx=20, pady=5)
 
+        # botao para enviar mensagem
         self.send_button = tkinter.Button(
             self.win, text="Enviar", command=self.write)
         self.send_button.pack(padx=20, pady=5)
 
         self.gui_done = True
 
+        # ao fechar janela chamar stop
         self.win.protocol("WM_DELETE_WINDOW", self.stop)
         self.win.mainloop()
 
     def write(self):
-        # message = f"{self.nickname}: {self.input_text.get('1.0', 'end')}"
+        # pegar mensagem do input
         message = f"{self.nickname}: {self.input_text.get()}"
+        # enviar mensagem
         self.socket.send(message.encode('utf-8'))
+        # limpar campo de texto
         self.input_text.delete(first=0, last='end')
-        # self.input_text.delete('1.0', 'end')
 
     def stop(self):
-        self.running = False
-        self.win.destroy()
-        self.socket.close()
-        exit(0)
+        self.running = False    # parar loop
+        self.win.destroy()      # fechar janela
+        self.socket.close()     # fechar socket
+        exit()
 
     def receive(self):
         while self.running:
             try:
                 message = self.socket.recv(1024).decode('utf-8')
                 if message == 'NICK':
-                    self.socket.send(message.encode('utf-8'))
+                    self.socket.send(self.nickname.encode(
+                        'utf-8'))  # enviar nickname
                 else:
                     if self.gui_done:
+                        # adicionar mensagem ao chat
                         self.chat_text.config(state='normal')
                         self.chat_text.insert('end', message + '\n')
                         self.chat_text.yview('end')
@@ -90,8 +106,9 @@ class Client:
                 break
             except:
                 print("Error")
-                self.sock.close()
+                self.sock.close()   # fechar socket
                 break
 
 
+# iniciar cliente
 client = Client(HOST, PORT)
--- server.py
+++ server.py
@@ -1,50 +1,72 @@
 import socket
 import threading
 
+# Declarar ip host e porta
 HOST = '127.0.0.1'
 PORT = 9090
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))
+# etapas da conexao tcp
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    # criar socket
+server.bind((HOST, PORT))  # bind
 
+# servidor espera por conexões
 server.listen(5)
 
 clients = []
 nicknames = []
 
+# funcao para enviar mensagens para todos os clientes
+
+
 def broadcast(message):
     for client in clients:
         client.send(message)
 
+# funcao para tratar as mensagens
+
+
 def handle(client):
     while True:
         try:
-            message = client.recv(1024)
-            print(f"{nicknames[clients.index(client)]}: {message}")
+            message = client.recv(1024)     # receber mensagem
+            print(message)                  # print log da mensagem
+
+            # enviar mensagem para todos os clientes
             broadcast(message)
         except:
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            nickname = nicknames[index]
-            nicknames.remove(nickname)
+            # se o cliente sair, remove-lo da lista
+
+            index = clients.index(client)   # pegar indice do cliente
+            clients.remove(client)          # remover cliente da lista
+            client.close()                  # fechar conexão
+            nickname = nicknames[index]     # pegar apelido do cliente
+            nicknames.remove(nickname)      # remover apelido da lista
             break
+
 
 def receive():
     while True:
+
+        # aceitar conexão e receber endereços
         client, adress = server.accept()
-        print(f'Connected to {str(adress)}')
+        print(f'Conectado em {str(adress)}')
+
+        # pegar apelido dp cliente e adicionar a lista de apelidos
         client.send("NICK".encode('utf-8'))
         nickname = client.recv(1024).decode('utf-8')
+        print(f'Apelido: {nickname}')
         nicknames.append(nickname)
         clients.append(client)
-        print(f'Nickname of {nickname} is added to the list')
-        broadcast(f'{nickname} has joined the chat!'.encode('utf-8'))
-        client.send('Connected to the server!'.encode('utf-8'))
 
+        print(f'Usuario {nickname} foi adicionado a lista')  # print log
+        # enviar mensagem de entrada para todos os clientes
+        broadcast(f'{nickname} entrou no chat!'.encode('utf-8'))
+
+        # criar thread para tratar mensagens
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
 
+
 # start the server
-print('Server is running...')
+print('Server esta rodando...')
 receive()
